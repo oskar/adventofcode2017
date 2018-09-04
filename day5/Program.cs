a7c16941f4e1d49f9534c3b5805f10247433405e
Oskar Hermansson <oskar@pamind.se>
2018-09-04 16:10:31 +0200

    day5**

---
+++ day5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var jumpOffsets = File.ReadLines("input.txt").Select(s => Convert.ToInt32(s)).ToArray();
+      System.Console.WriteLine("Jump offsets: " + jumpOffsets.Length);
+      var numberOfJumps = Jumper(jumpOffsets);
+      System.Console.WriteLine("Number of jumps: " + numberOfJumps);
+    }
+
+    static int Jumper(int[] jumpOffsets)
+    {
+      var index = 0;
+      var numberOfJumps = 0;
+
+      while (index >= 0 && index < jumpOffsets.Length)
+      {
+        var offset = jumpOffsets[index];
+        if (offset > 2)
+        {
+          jumpOffsets[index]--;
+        }
+        else
+        {
+          jumpOffsets[index]++;
+        }
+        index += offset;
+        numberOfJumps++;
+      }
+
+      return numberOfJumps;
+    }
+  }
+}
